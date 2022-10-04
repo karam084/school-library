681114ccc30a91db7c106750d759d9464e477752
karam084 <karam.ezz84@gmail.com>
2022-10-05 00:32:13 +0200

    Fix linter

--- Person.rb
+++ Person.rb
@@ -1,21 +1,21 @@
 class Person
     def initialize(age, id = 0, name = 'Unknown', parent_permission: true)
-      @id = id
-      @age = age
-      @name = name
-      @parent_permission = parent_permission
+    @id = id
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
     end
-  
-    attr_reader :id
-    attr_accessor :age, :name
-  
-    def can_use_services?
-      of_age? || @parent_permission
+    
+    attr_reader:id
+    attr_accessor:age,:name
+    
+    def can_use_services ?
+      of_age ? || @parent_permission
     end
-  
+    
     private
-  
-    def of_age?
+    
+    def of_age ?
       @age >= 18
     end
-  end
+    end
