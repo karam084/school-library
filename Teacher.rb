9151cb2f696c125cef99195b6ff11fe8ca9a5fc4
karam084 <karam.ezz84@gmail.com>
2022-10-04 22:56:22 +0200

    Add teacher class

---
+++ Teacher.rb
@@ -0,0 +1,12 @@
+require_relative 'person'
+
+class Teacher < Person
+  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
+    super(age: age, parent_permission: parent_permission, name: name)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
