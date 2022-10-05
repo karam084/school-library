ffc5a4f51d9f8100c46b228649112d1580a2b994
Karam Fouad <karam.ezz84@gmail.com>
2022-10-05 09:33:31 +0200

    Update person.rb
    Add private method

--- person.rb
+++ person.rb
@@ -1,7 +1,7 @@
 # This is the Person class, parent of student and teacher class
 class Person
   def initialize(age, id = 0, name = 'Unknown', parent_permission: true)
-    @id = id
+    @id = Random.rand(1..1000)
     @age = age
     @name = name
     @parent_permission = parent_permission
@@ -12,6 +12,8 @@ class Person
   def can_use_services?
     of_age? || @parent_permission
   end
+  
+  private
 
   def of_age?
     @age >= 18
