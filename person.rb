01e8e9c32db04829895e677bc37debefdcae443e
Karam Fouad <karam.ezz84@gmail.com>
2022-10-05 10:50:04 +0200

    Update person.rb

--- person.rb
+++ person.rb
@@ -1,21 +1,22 @@
 # This is the Person class, parent of student and teacher class
 class Person
+  attr_accessor :name, :age
+  attr_reader :id
+
   def initialize(age:, name: 'Unknown', parent_permission: true)
-    @id = Random.rand(1..1000)
-    @age = age
+    @id = Random.rand(1..100)
     @name = name
+    @age = age
     @parent_permission = parent_permission
   end
-  attr_reader :id
-  attr_accessor :age, :name
 
   def can_use_services?
     of_age? || @parent_permission
   end
-  
+
   private
 
- def of_age ? @age >= 18
- end
- end
- 
+  def of_age?
+    @age >= 18
+  end
+end
