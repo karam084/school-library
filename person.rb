961ce7d99965a4f65ddc480f26fa5710a6a9f7ef
karam084 <karam.ezz84@gmail.com>
2022-10-07 22:36:32 +0200

    Add many-to-many to book, person, and rental

--- person.rb
+++ person.rb
@@ -12,6 +12,7 @@ class Person < Nameable
     @age = age
     @name = name
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   attr_reader :id
--- student.rb
+++ student.rb
@@ -3,16 +3,16 @@ require_relative 'Person'
 class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age: age, parent_permission: parent_permission, name: name)
-    @classroom = classroom
+    write_classroom(classroom)
   end
 
   def play_hooky
     '-\\("/)/-'
   end
-  
+
   def write_classroom(classroom)
     @classroom = classroom
-    classroom.add_student(self)
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
 
