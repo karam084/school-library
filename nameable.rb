584e959c5fdafb36e59974d589df819957ff70a1
karam084 <karam.ezz84@gmail.com>
2022-10-12 02:00:59 +0200

    Solve errors

--- nameable.rb
+++ nameable.rb
@@ -1,5 +1,5 @@
 class Nameable
   def correct_name
-    raise NotImplementedError, 'Please implement The correct_name'
+    raise NotImplementedError, 'Please implement correct_name'
   end
 end
--- student.rb
+++ student.rb
@@ -1,7 +1,9 @@
 require './person'
 # This is the Person class, parent of student and teacher class
 class Student < Person
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+  attr_reader :classroom
+
+  def initialize(age:, classroom:, name: 'Unknown', parent_permission: true)
     super(age: age, parent_permission: parent_permission, name: name)
     write_classroom(classroom)
   end
@@ -12,6 +14,6 @@ class Student < Person
 
   def write_classroom(classroom)
     @classroom = classroom
-    classroom.students.push(self) unless classroom.students.include?(self)
+    classroom.add_student(self)
   end
 end
